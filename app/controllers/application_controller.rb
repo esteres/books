5c7634653b54413b8ef2005df9fbb25ef902e8fb
esteres <restreporamirezesteban4@gmail.com>
2024-04-12 18:16:36 -0500

    Error handling for DELETE action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: not_destroyed
+
+  private
+
+  def not_destroyed(error)
+    render json: { errors: erro.record.errros }, status: :unprocessable_entity
+  end
 end
