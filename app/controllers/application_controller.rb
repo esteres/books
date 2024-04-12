2c6405650acbd3f2ced5ff91e67c6071f7021904
esteres <restreporamirezesteban4@gmail.com>
2024-04-12 18:30:41 -0500

    Namespacing and versioning api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound, with: not_destroyed
+  rescue_from ActiveRecord::RecordNotFound, with: :not_destroyed
 
   private
 
